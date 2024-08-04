5a77148919d8d51915a93e0891a6e4138734a7d3
chang <changwskr@gmail.com>
2024-08-04 14:47:35 +0900

    local first

--- src/main/java/com/example/h3demo/Ih6demo20240802Application.java
+++ src/main/java/com/example/h3demo/Ih6demo20240802Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class  Ih6demo20240802Application {
 
+
+	/**
+	 * test kkkkkk
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Ih6demo20240802Application.class, args);
 	}
